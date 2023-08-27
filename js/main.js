246967256f5cd3747eab6b2a3e303f7fafcaaecf
Manoj C <49671622+manojcgit@users.noreply.github.com>
2023-08-27 20:56:37 +0530

    Added all files

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+
+$(document).ready(function() {
+  AOS.init( {
+    // uncomment below for on-scroll animations to played only once
+    // once: true  
+  }); // initialize animate on scroll library
+});
+
+// Smooth scroll for links with hashes
+$('a.smooth-scroll')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
+
+//Automatic multiple slides on same page
+var slideshowContainers = document.getElementsByClassName("cc-porfolio-image");
+for(let s = 0; s < slideshowContainers.length; s++) {    
+    var cycle = slideshowContainers[s].dataset.cycle;
+    var slides = slideshowContainers[s].querySelectorAll('.projectSlides');
+    var slideIndex = 0;
+    showSlides(slides, slideIndex, cycle);
+};
+
+function showSlides(slides, slideIndex, cycle) {
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    };
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    };
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(function() {
+        showSlides(slides, slideIndex, cycle)
+    }, cycle);
+};
+
